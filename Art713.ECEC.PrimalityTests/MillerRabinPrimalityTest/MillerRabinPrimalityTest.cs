a6b7778bcd5d9319ec238c853cc6d86f8666506f
makaveli713 <makaveli713@yandex.ua>
2013-03-15 12:19:28 +0200

    issue #1 and issue #2 is done now.

---
+++ Art713.ECEC.PrimalityTests/MillerRabinPrimalityTest/MillerRabinPrimalityTest.cs
@@ -0,0 +1,67 @@
+﻿using System.Numerics;
+
+namespace Art713.ECEC.PrimalityTests.MillerRabinPrimalityTest
+{
+    public class Program
+    {
+        static void Main()
+        {
+            System.Console.WriteLine("Art713.ECEC.PrimalityTests.MillerRabinPrimalityTest");
+            System.Console.ReadLine();
+        }
+    }
+
+    public interface IPrimalityTest
+    {
+        bool MillerRabinPrimalityTest(BigInteger number);
+    }
+
+    public class PrimalityTest : IPrimalityTest
+    {
+        public bool MillerRabinPrimalityTest(BigInteger number)
+        {
+            if (number == 2) return true;
+
+            double t = BigInteger.Log(number, 2.0);
+
+            BigInteger m = BigInteger.Subtract(number, 1);
+
+            if (BigInteger.ModPow(2, m, number) != 1) return false;
+
+            int s = 0;
+            bool flag = true;
+
+            do
+            {
+                if (m % 2 == 0)
+                {
+                    m = BigInteger.Divide(m, 2);
+                    s++;
+                }
+                else
+                    flag = false;
+            } while (flag);
+
+            BigInteger a = 1;
+            for (int j = 0; j < t; j++)
+            {
+                ++a;
+                if (a > number - 1) return false;
+                BigInteger b = BigInteger.ModPow(a, m, number);
+                if (b != 1)
+                {
+                    for (int i = 1; i <= s; i++)
+                    {
+                        if (b == (number - 1))
+                        {
+                            return true; //prime!
+                        }
+                        b = BigInteger.ModPow(b, 2, number);
+                    }
+                    return false; //composite!
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Art713.ECEC.TestOfPrimalityTest/TestOfMillerRabinPrimalityTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+using Art713.ECEC.PrimalityTests.MillerRabinPrimalityTest;
+using NUnit.Framework;
+
+namespace Art713.ECEC.TestOfPrimalityTest
+{
+    class Program
+    {
+         static void Main()
+         {
+         }
+    }
+
+    [TestFixture]
+    class TestOfPrimalityTest
+    {
+        private PrimalityTest _setup;
+        public static string[] PrimeNumberStrings { get; set; }
+        private static readonly Dictionary<BigInteger, bool> TestDataField = new Dictionary<BigInteger, bool>();
+
+        public static Dictionary<BigInteger, bool> TestData
+        {
+            get
+            {
+                try
+                {
+                    const string path = @"C:\Users\Art\Documents\Visual Studio 2012\Projects\Repository\Art713.ECEC\prime numbers.txt";
+                    using (var sr = new StreamReader(path))
+                    {
+                        var allnumbers = sr.ReadToEnd();
+                        PrimeNumberStrings = allnumbers.Split(' ');
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("The file could not be read:");
+                    Console.WriteLine(e.Message);
+                }
+
+                foreach (var primenum in PrimeNumberStrings.Where(primenum => TestDataField != null).Where(primenum => TestDataField != null))
+                {
+                    TestDataField.Add(BigInteger.Parse(primenum), true);
+                    TestDataField.Add((BigInteger.Parse(primenum)) * 2, false);
+                }
+                return TestDataField;
+            }
+        }
+
+        [TestFixtureSetUp]
+        public void SetUpBeforeEveryTestFunction()
+        {
+            _setup = new PrimalityTest();
+        }
+
+        [Test,
+        TestCaseSource("TestData")]
+        public void ShouldReturnTrueIfNumberIsPrime(KeyValuePair<BigInteger, bool> keyValuePair)
+        {
+            Assert.That(_setup.MillerRabinPrimalityTest(keyValuePair.Key), Is.EqualTo(keyValuePair.Value));
+        }
+    }
+}
