8be6b182940081d3d273cb3dc6ce2cc333603b0d
makaveli713 <makaveli713@yandex.ua>
2013-03-26 22:38:54 +0200

    processing issue #3

---
+++ Art713.ECEC.TestOfPrimalityTest/TestOfPointAddition.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace Art713.ECEC.TestOfPrimalityTest
+{
+    [TestFixture]
+    class TestOfPointAddition
+    {
+        private Point _setup;
+
+        [TestFixtureSetUp]
+        public void SetupBeforeEveryTestFunction()
+        {
+            _setup = new Point();
+        }
+
+        [Test]
+        public void ShouldReturnPointWhenAddingOtherTwoPoint()
+        {
+            Point firstPoint = new Point(5,1);
+            Point secondPoint = new Point(4,6);
+            Assert.That(_setup.PointAddition(firstPoint,secondPoint),Is.EqualTo(new Point(2,5)));
+        }
+    }
+}
