08a371bc1a38519ecfe05aedb4486a488db482c4
makaveli713 <makaveli713@yandex.ua>
2013-03-27 00:31:53 +0200

    issue #7 is done.

---
+++ Art713.ECEC/Auxiliary/Math.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Art713.ECEC.Auxiliary
+{
+    static class Math
+    {
+        public static int Mod(int a, int b)
+        {
+            if (a < 0)
+                return b + (a % b);
+            return a % b;
+        }
+    }
+}
