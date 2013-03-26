851d77add6836fe389b99dc2a302b66b934ffbbb
makaveli713 <makaveli713@yandex.ua>
2013-03-27 01:22:26 +0200

    issue #9 is done.

--- Art713.ECEC.TestOfPrimalityTest/TestOfPointAddition.cs
+++ Art713.ECEC.TestOfPrimalityTest/TestOfPointAddition.cs
@@ -11,17 +11,26 @@ namespace Art713.ECEC.TestOfPrimalityTest
         [TestFixtureSetUp]
         public void SetupBeforeEveryTestFunction()
         {
-            _setup = new EllipticCurve(2,6,7);
+            _setup = new EllipticCurve(2, 6, 7);
         }
 
         [Test]
         public void ShouldReturnPointWhenAddingOtherTwoPoint()
         {
-            var firstPoint = new Point(5,1);
-            var secondPoint = new Point(4,6);
+            var firstPoint = new Point(5, 1);
+            var secondPoint = new Point(4, 6);
             var sumPoint = new Point(2, 5);
-            Assert.That(_setup.PointAddition(firstPoint,secondPoint).Abscissa,Is.EqualTo(sumPoint.Abscissa));
+            Assert.That(_setup.PointAddition(firstPoint, secondPoint).Abscissa, Is.EqualTo(sumPoint.Abscissa));
             Assert.That(_setup.PointAddition(firstPoint, secondPoint).Ordinate, Is.EqualTo(sumPoint.Ordinate));
+        }
+        [Test]
+        public void ShouldReturnPointWhenDoublingPoint()
+        {
+            var firstPoint = new Point(5, 1);
+            var doublePoint = new Point(4, 6);            
+            Assert.That(_setup.PointDoubling(firstPoint).Abscissa, Is.EqualTo(doublePoint.Abscissa));
+            Assert.That(_setup.PointDoubling(firstPoint).Ordinate, Is.EqualTo(doublePoint.Ordinate));
+        
         }
     }
 }
