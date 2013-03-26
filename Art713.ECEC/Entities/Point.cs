c3a45a261e7ac6ebae680043ed38de3f50778853
makaveli713 <makaveli713@yandex.ua>
2013-03-26 23:06:24 +0200

    issue #5. Created class Point. Point coordinates is integer now.

---
+++ Art713.ECEC/Entities/Point.cs
@@ -0,0 +1,17 @@
+﻿namespace Art713.ECEC.Entities
+{
+    /// <summary>
+    /// class Point represent a point on elliptic curve
+    /// </summary>
+    public class Point
+    {
+        /// <summary>
+        /// first coordinate of the point
+        /// </summary>
+        public int XPoint { get; set; } // int to BigInteger ?
+        /// <summary>
+        /// second coordinate of the point
+        /// </summary>
+        public int YPoint { get; set; } // int to BigInteger ?
+    }
+}
