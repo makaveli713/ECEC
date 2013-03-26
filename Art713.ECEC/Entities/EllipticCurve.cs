ea9038bf51b291fce711046f941d988afd825e47
makaveli713 <makaveli713@yandex.ua>
2013-03-26 23:05:02 +0200

    Issue #4 is processing. Created class EllipticCurve, added properties and methods, but methods is not implemented yet

---
+++ Art713.ECEC/Entities/EllipticCurve.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace Art713.ECEC.Entities
+{
+    /// <summary>
+    /// class EllipticCurve represent a elliptic curve
+    /// </summary>
+    public class EllipticCurve
+    {
+        // properties:
+
+        /// <summary>
+        /// "A" is a first parameter of the elliptic curve
+        /// </summary>
+        public int A { get; set; }
+        /// <summary>
+        /// "B" is a second parameter of the elliptic curve
+        /// </summary>
+        public int B { get; set; }
+        /// <summary>
+        /// "P" is a modulus, third parameter of the elliptic curve
+        /// </summary>
+        public int P { get; set; }
+        /// <summary>
+        /// "Points" is a list objects of class Point
+        /// </summary>
+        public List<Point> Points { get; set; }
+        
+        // methods:
+
+
+        public void FillPoint()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Point PointAddition(Point fPoint, Point sPoint)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Point PointDoubling(Point point)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Point PointMultiplication(Point point, int n)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
