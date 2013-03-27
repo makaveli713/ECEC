7eecfd645be8c0ce03024b03c7df163e58525f1f
makaveli713 <makaveli713@yandex.ua>
2013-03-27 11:40:07 +0200

    issue #11 is done.

--- Art713.ECEC/Auxiliary/Math.cs
+++ Art713.ECEC/Auxiliary/Math.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,5 +15,16 @@ namespace Art713.ECEC.Auxiliary
                 return b + (a % b);
             return a % b;
         }
+        
+        public static byte[] GetBits(int number)
+        {
+            //var k = (number/255 == 0) ? 1 : (number%255 == 0) ? number/255:number/255 + 1;
+            var k = (number/255 == 0) ? 0 : number/255; //(number % 255 == 0) ? number / 255 : number / 255 + 1;
+            var bytes = new byte[8+k];
+            for (var i = 0; i < 8+k; ++i)
+                bytes[i] = Byte.Parse(((number >> i) & 1).ToString(CultureInfo.InvariantCulture));
+            return bytes;
+        }
+        
     }
 }
