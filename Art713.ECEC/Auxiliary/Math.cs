11cfb6d49fa87b5ca75cd1f3595922f259144514
makaveli713 <makaveli713@yandex.ua>
2013-03-27 22:50:23 +0200

    made class Math public

--- Art713.ECEC/Auxiliary/Math.cs
+++ Art713.ECEC/Auxiliary/Math.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Art713.ECEC.Auxiliary
 {
-    static class Math
+    public static class Math
     {
         public static int Mod(int a, int b)
         {
@@ -18,13 +18,11 @@ namespace Art713.ECEC.Auxiliary
         
         public static byte[] GetBits(int number)
         {
-            //var k = (number/255 == 0) ? 1 : (number%255 == 0) ? number/255:number/255 + 1;
-            var k = (number/255 == 0) ? 0 : number/255; //(number % 255 == 0) ? number / 255 : number / 255 + 1;
+            var k = (number/255 == 0) ? 0 : number/255; 
             var bytes = new byte[8+k];
             for (var i = 0; i < 8+k; ++i)
                 bytes[i] = Byte.Parse(((number >> i) & 1).ToString(CultureInfo.InvariantCulture));
             return bytes;
-        }
-        
+        }        
     }
 }
