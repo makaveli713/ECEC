f1b658b3361dd623c36d9e254226c361bab3de14
makaveli713 <makaveli713@yandex.ua>
2013-04-04 21:40:49 +0300

    created ISecureMessagingService interface and ISecureMessagingServiceCallback interface
    (using Duplex MEP)

---
+++ SecureMessagingService/ISecureMessagingService.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace SecureMessagingService
+{
+    [ServiceContract(CallbackContract = typeof(ISecureMessagingServiceCallback))]
+    public interface IService1
+    {
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+
+    }
+}
