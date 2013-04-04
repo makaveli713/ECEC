b846693887befbaa1b3f5c52e1f379bcc3c9ab0f
makaveli713 <makaveli713@yandex.ua>
2013-04-04 22:04:54 +0300

    implement contract

---
+++ SecureMessagingService/SecureMessagingService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SecureMessagingService
+{
+    public class SecureMessagingService : ISecureMessagingService
+    {
+        public string Encrypt(string textToEncrypt)
+        {
+            var obj = new Art713.ECEC.Cryptography.Encryption();
+            return obj.Encrypt(textToEncrypt);
+        }
+
+        public string Decrypt(string textToDecrypt)
+        {
+            var obj = new Art713.ECEC.Cryptography.Encryption();
+            return obj.Decrypt(textToDecrypt);
+        }
+    }
+}
