16adbe61b94457a548e35fc454e192f73e1275e9
makaveli713 <makaveli713@yandex.ua>
2013-05-07 14:03:30 +0300

    Closes #40. Added MemoryRandomAccessStream class

---
+++ Art713.Project713/Auxiliary/MemoryRandomAccessStream.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using Windows.Foundation;
+using Windows.Storage.Streams;
+
+namespace Art713.Project713.Auxiliary
+{
+    class MemoryRandomAccessStream : IRandomAccessStream
+    {
+        private Stream _stream;
+
+        public MemoryRandomAccessStream(Stream stream)
+        {
+            _stream = stream;
+        }
+
+        public MemoryRandomAccessStream(byte[] bytes)
+        {
+            _stream = new MemoryStream(bytes);
+        }
+
+        public void Dispose()
+        {
+            _stream.Dispose();
+        }
+
+        public IAsyncOperationWithProgress<IBuffer, uint> ReadAsync(IBuffer buffer, uint count, InputStreamOptions options)
+        {
+            var inputStream = GetInputStreamAt(0);
+            return inputStream.ReadAsync(buffer, count, options);
+        }
+
+        public IAsyncOperationWithProgress<uint, uint> WriteAsync(IBuffer buffer)
+        {
+            var outputStream = GetOutputStreamAt(0);
+            return outputStream.WriteAsync(buffer);
+        }
+
+        public IAsyncOperation<bool> FlushAsync()
+        {
+            var outputStream = GetOutputStreamAt(0);
+            return outputStream.FlushAsync();
+        }
+
+        public IInputStream GetInputStreamAt(ulong position)
+        {
+            _stream.Seek((long) position, SeekOrigin.Begin);
+            return _stream.AsInputStream();
+        }
+
+        public IOutputStream GetOutputStreamAt(ulong position)
+        {
+            _stream.Seek((long)position, SeekOrigin.Begin);
+            return _stream.AsOutputStream();
+        }
+
+        public void Seek(ulong position)
+        {
+            _stream.Seek((long)position, 0);
+        }
+
+        public IRandomAccessStream CloneStream()
+        {
+            throw new NotSupportedException();
+        }
+
+        public bool CanRead
+        {
+            get { return true; }
+        }
+
+        public bool CanWrite
+        {
+            get { return true; }
+        }
+
+        public ulong Position 
+        {
+            get { return (ulong)_stream.Position; }
+        }
+
+        public ulong Size 
+        { 
+            get { return (ulong) _stream.Length; }
+            set { _stream.SetLength((long)value); }
+        }
+    }
+}
--- Art713.Project713/MainPage.xaml.cs
+++ Art713.Project713/MainPage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.IO;
 using System.Text;
 using Art713.Project713.Cryptography;
 using Windows.Foundation;
+using Windows.Graphics.Imaging;
 using Windows.Storage;
 using Windows.Storage.Pickers;
 using Windows.Storage.Streams;
@@ -48,102 +50,132 @@ namespace Art713.Project713
         {
             DecryptedTextBlock.Text = EncryptionObj.Decrypt(EncryptedTextBlock.Text);
             DecryptBtn.Foreground = DecryptedTextBlock.Text == TextToEncrypt.Text ? new SolidColorBrush(Colors.Green) : new SolidColorBrush(Colors.Red);
+
+
+            //IRandomAccessStream fileStream = Encoding.UTF8.GetBytes(DecryptedTextBlock.Text);
+
+            MemoryStream memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(DecryptedTextBlock.Text));
+
+            var bitmapImage = new BitmapImage();
+            bitmapImage.SetSource(fileStream);
+            var image = new Image
+            {
+                Source = bitmapImage,
+                Width = 100,
+                Height = 100,
+                Visibility = Visibility.Visible,
+                HorizontalAlignment = HorizontalAlignment.Left,
+                VerticalAlignment = VerticalAlignment.Top,
+                Margin = new Thickness(0, 0, 0, 0)
+            };
+            // */
+
         }
 
         private async void OpenBtn_OnClick(object sender, RoutedEventArgs e)
-        {      
-            var openPicker = new FileOpenPicker {SuggestedStartLocation = PickerLocationId.Desktop};
+        {
+            var openPicker = new FileOpenPicker { SuggestedStartLocation = PickerLocationId.Desktop };
             openPicker.FileTypeFilter.Add("*");
 
             var file = await openPicker.PickSingleFileAsync();
 
             if (file != null)
             {
+                IRandomAccessStream fileStream = await file.OpenAsync(FileAccessMode.Read);
+
+                var dataReader = new DataReader(fileStream);
+                await dataReader.LoadAsync((uint)fileStream.Size); // this sets position of the stream to the end, so when i want to use it again i have to set position of the stream to the beggining!
+                var buffer = new byte[(int)fileStream.Size];
+                dataReader.ReadBytes(buffer);
+
+                fileStream.Seek(0); // sets position of the stream to 0
+
                 if (file.FileType == ".jpg")
                 {
                     var bitmapImage = new BitmapImage();
-                    using (IRandomAccessStream fileStream = await file.OpenAsync(FileAccessMode.Read))
-                    {
-                        bitmapImage.SetSource(fileStream);
-                        var image = new Image
-                            {
-                                Source = bitmapImage,
-                                Width = 100,
-                                Height = 100,
-                                Visibility = Visibility.Visible,
-                                HorizontalAlignment = HorizontalAlignment.Left,
-                                VerticalAlignment = VerticalAlignment.Top
-                            };
-                        var thickness = new Func<Thickness>(()=> ImagesGrid.Children.Count*100 < 400 ? new Thickness(ImagesGrid.Children.Count*100, 0, 0, 0) : new Thickness(0, 100, 0, 0)); // CHANGE!
-                        image.Margin = thickness();                    
-                        ImagesGrid.Children.Insert(ImagesGrid.Children.Count, image);
+                    bitmapImage.SetSource(fileStream);
+                    var image = new Image
+                        {
+                            Source = bitmapImage,
+                            Width = 100,
+                            Height = 100,
+                            Visibility = Visibility.Visible,
+                            HorizontalAlignment = HorizontalAlignment.Left,
+                            VerticalAlignment = VerticalAlignment.Top,
+                            Margin = new Thickness(0, 0, 0, 0)
+                        };
+                    /*var thickness =
+                        new Func<Thickness>(
+                            () =>
+                            ImagesGrid.Children.Count * 100 < 400
+                                ? new Thickness(ImagesGrid.Children.Count * 100, 0, 0, 0)
+                                : new Thickness(0, 100, 0, 0)); // CHANGE!
+                    image.Margin = thickness();
+                     */
+                    ImagesGrid.Children.Insert(0, image);
 
-                    }
+                    var encoding = Encoding.UTF8;
+                    TextToEncrypt.Text = encoding.GetString(buffer, 0, buffer.Length);
+
                 }
-                if (file.FileType == ".txt")
-                {
-                    using (IRandomAccessStream fileStream = await file.OpenAsync(FileAccessMode.Read))
-                    {
-                        var dataReader = new DataReader(fileStream);
-                        await dataReader.LoadAsync((uint)fileStream.Size);
-                        var buffer = new byte[(int)fileStream.Size];
-                        dataReader.ReadBytes(buffer);
-                        var encoding = Encoding.UTF8;
-                        TextToEncrypt.Text = encoding.GetString(buffer, 0, buffer.Length);
-                    }
-                }
-            }
-
-
-            /*
-            if (file != null)
-            {
-                IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.Read);
-
-                var dataReader = new DataReader(stream);
-                    await dataReader.LoadAsync((uint)stream.Size);
-                var buffer = new byte[(int)stream.Size];
-                dataReader.ReadBytes(buffer);
-
                 if (file.FileType == ".txt")
                 {
                     var encoding = Encoding.UTF8;
                     TextToEncrypt.Text = encoding.GetString(buffer, 0, buffer.Length);
                 }
+            }
+        }
 
-                if (file.FileType == ".jpg")
-                {                    
-                    var bitmap = new BitmapImage();
-                    bitmap.SetSource(stream);                  
 
-                    MyImage.Visibility = Visibility.Visible;
-                    MyImage.Stretch = Stretch.None;
-                    MyImage.Source = bitmap;
+        /*
+        if (file != null)
+        {
+            IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.Read);
+
+            var dataReader = new DataReader(stream);
+                await dataReader.LoadAsync((uint)stream.Size);
+            var buffer = new byte[(int)stream.Size];
+            dataReader.ReadBytes(buffer);
+
+            if (file.FileType == ".txt")
+            {
+                var encoding = Encoding.UTF8;
+                TextToEncrypt.Text = encoding.GetString(buffer, 0, buffer.Length);
+            }
+
+            if (file.FileType == ".jpg")
+            {                    
+                var bitmap = new BitmapImage();
+                bitmap.SetSource(stream);                  
+
+                MyImage.Visibility = Visibility.Visible;
+                MyImage.Stretch = Stretch.None;
+                MyImage.Source = bitmap;
 
                    
 
-                    var image = new Image
-                        {
-                            Source = bitmap,
-                            Width = 200,
-                            Height = 200,
-                            Visibility = Visibility.Visible,
-                            Margin = new Thickness(10, 10, 0, 0)
-                        };
+                var image = new Image
+                    {
+                        Source = bitmap,
+                        Width = 200,
+                        Height = 200,
+                        Visibility = Visibility.Visible,
+                        Margin = new Thickness(10, 10, 0, 0)
+                    };
 
-                    //image.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
-                    //image.VerticalAlignment = System.Windows.VerticalAlignment.Top;
-                    //Point tappedPoint = e.GetPosition(contentGrid);
-                    //contentGrid.Children.Add(image);
+                //image.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+                //image.VerticalAlignment = System.Windows.VerticalAlignment.Top;
+                //Point tappedPoint = e.GetPosition(contentGrid);
+                //contentGrid.Children.Add(image);
 
-                    //MainGrid.Children.Add(image);
+                //MainGrid.Children.Add(image);
                       
                     
-                    //MainGrid.Children.Insert(0,image);
-                }
-             
+                //MainGrid.Children.Insert(0,image);
             }
-             */
+             
         }
+         */
     }
 }
+//}
